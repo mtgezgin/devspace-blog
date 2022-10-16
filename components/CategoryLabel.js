ccf1431ae37a64025e2965d6f3838d53127206ac
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-10-16 10:26:13 +0300

    Hotfix

--- components/CategoryLabel.js
+++ components/CategoryLabel.js
@@ -11,7 +11,7 @@ export default function CategoryLabel({ children }) {
 
 	return (
 		<div
-			className={`px-2 py-1 bg-${colorKey[children]}-600 text-black-100 font-bold rounded`}>
+			className={`px-2 py-1 bg-${colorKey[children]}-600 text-gray-100 font-bold rounded`}>
 			<Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
 		</div>
 	);
